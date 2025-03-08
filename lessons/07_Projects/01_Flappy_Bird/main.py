4be445f2cee3df2b5e370bb24ba0ea66b996026e
jayryu1234 <midori26b@gmail.com>
2025-03-08 01:34:30 +0000

    hj,hu

---
+++ lessons/07_Projects/01_Flappy_Bird/main.py
@@ -0,0 +1,15 @@
+import pygame
+import pathlib
+
+class Ground(pygame.sprite.Sprite):
+    def __init__(self):
+
+        self.picture = pathlib
+class Bird():
+    pass
+class Pipe():
+    pass
+def main():
+    pass
+if __name__ == "main":
+    main()
