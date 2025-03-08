49b627875c9797ebff2d189f8645fd4c158d5da8
jayryu1234 <midori26b@gmail.com>
2025-03-08 02:28:10 +0000

    turtjeryh

--- lessons/07_Projects/01_Flappy_Bird/main.py
+++ lessons/07_Projects/01_Flappy_Bird/main.py
@@ -1,10 +1,47 @@
-import pygame
-import pathlib
+import pygame, random, time
+from pygame.locals import *
+from pathlib import Path
 
+#VARIABLES
+SCREEN_WIDTH = 400
+SCREEN_HEIGHT = 600
+SPEED = 20
+GRAVITY = 2.5
+GAME_SPEED = 15
+BLACK = (0, 0, 0)
+GROUND_WIDTH = 2 * SCREEN_HEIGHT
+GROUND_HEIGHT= 100
+
+PIPE_WIDHT = 80
+PIPE_HEIGHT = 500
+
+PIPE_GAP = 150
+score = 0
+
+dd = Path(__file__).parent
+
+
+
+wing = dd/'assets/audio/wing.wav'
+hit = dd/'assets/audio/hit.wav'
 class Ground(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, xpos):
+        pygame.sprite.Sprite.__init__(self)
+        self.pic = pygame.image.load(dd/"assets/sprites/base.png").convert_alpha()
+        self.pic = pygame.transform.scale(self.image, (SCREEN_WIDTH, SCREEN_HEIGHT))
 
-        self.picture = pathlib
+        self.mask = pygame.mask.from_surface(self.image)
+
+        self.rect = self.pic.get_rect()
+        self.rect[0] = xpos
+        self.rect[1] = SCREEN_HEIGHT - GROUND_HEIGHT
+
+    def update(self):
+        self.rect[0] -= GAME_SPEED
+
+def is_offscreenprob(sprite):
+    return sprite.rect[0] < -sprite[2]/2
+
 class Bird():
     pass
 class Pipe():
@@ -13,3 +50,4 @@ def main():
     pass
 if __name__ == "main":
     main()
+
